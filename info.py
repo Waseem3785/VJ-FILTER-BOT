2ffe5dd3edd2d94690d55df6794a608e3cbb0754
Waseem3785 <169117544+Waseem3785@users.noreply.github.com>
2024-05-10 09:50:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/c16dca0f244857794a4
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ' -1002078336217'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6339373118').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1885130150').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002056148624').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '-1001999136212').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
